fbffa502c62cab4f7fc7a539cde57fe51fb6826f
NathanVienne <nathan.vienne@outlook.fr>
2025-04-25 11:46:24 +0400

    TailwindCSS integration and configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ function App() {
   return (
     <>
       <div>
-        <h1>Bienvenue sur CardAdvantage !</h1>
+        <h1 className="text-dark font-inter">Bienvenue sur CardAdvantage !</h1>
       </div>
+      <h2 className="text-secondary font-poppins">TEST POPPINS</h2>
+      <h2 className="text-secondary font-inter">TEST INTER</h2>
     </>
   )
 }
