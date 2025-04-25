34df86675efd9681bfab2081ce6b49fc589070d6
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-04-25 14:50:42 +0400

    feat: add offers table with soft deletes

---
+++ database/migrations/2025_04_25_081257_creat_offers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('offers', function(Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('is_active');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('category')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
