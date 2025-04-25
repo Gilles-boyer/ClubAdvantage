7ee873213cf2f93136611ba754ea36eb69bc2c9a
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-04-25 16:32:27 +0400

    feat: add users table with soft deletes

---
+++ database/migrations/2025_04_25_120431_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->dropUnique('email');
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('terms_accepted_at')->nullable();
+            $table->enum('status', ['active', 'inactive', 'expired']);
+            $table->bigInteger('role_id');
+            $table->string('remember_token')->nullable();
+            $table->unsignedBigInteger('committee_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('committee_id')->references('id')->on('committees');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
