46c03b44ac912094fd171ae8ea689350abf5bdf7
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-04-28 12:27:08 +0400

    feat: add committee_offer pivot table

---
+++ database/migrations/2025_04_28_080535_create_committee_offer_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('committee_offer', function (Blueprint $table) {
+            $table->unsignedBigInteger('committee_id');
+            $table->unsignedBigInteger('offer_id');
+        
+            $table->primary(['committee_id', 'offer_id']);
+        
+            $table->foreign('committee_id')->references('id')->on('committees')->onDelete('cascade');
+            $table->foreign('offer_id')->references('id')->on('offers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('committee_offer');
+    }
+};
