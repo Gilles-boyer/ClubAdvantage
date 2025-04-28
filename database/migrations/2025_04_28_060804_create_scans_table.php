c30b63f31b5559ff2bae74defbf882c038b8bd19
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-04-28 10:43:10 +0400

    feat: add scans table

---
+++ database/migrations/2025_04_28_060804_create_scans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scans', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('scanned_at');
+            $table->unsignedBigInteger('scanned_by')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            // L'utilisateur STAFF qui fait un scan
+            $table->foreign('scanned_by')->references('id')->on('users');
+            // L'utilisateur Membre du CSE / CSE qui se fait scanner
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scans');
+    }
+};
