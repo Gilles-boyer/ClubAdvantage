136df62a6f9f64e81c152a690f5e2dcbaef9848f
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-04-28 10:03:04 +0400

    create User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,29 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Authenticatable
+class User extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
+        'email_verified_at',
+        'terms_accepted_at',
+        'status',
+        'role_id',
+        'committee_id',
         'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+ // Dit à laravel que c'est champs doivent être manipulés comme des dates automatique
+    protected $casts = [
+        'email_verified_at' => 'datatime',
+        'terms_accepted_at' => 'datatime',
+        'deleted_at' => 'datetime',
+    ];
 }
