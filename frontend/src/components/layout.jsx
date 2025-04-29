0ecdfd5b529e1262a603fb2b11cd763e900332cb
NathanVienne <nathan.vienne@outlook.fr>
2025-04-29 12:19:04 +0400

    RAS

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Sidebar from "./Sidebar";
 import Header from "./header";
 import MainContent from "./MainContent";
@@ -7,19 +6,15 @@ import Footer from "./Footer"; // Si vous souhaitez un footer
 function Layout() {
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Sidebar à gauche */}
       <Sidebar />
 
-      {/* Contenu principal */}
       <div className="flex flex-col flex-1">
         <Header />
 
-        {/* Zone centrale du dashboard */}
         <main className="p-4 flex-1 overflow-auto">
           <MainContent />
         </main>
 
-        {/* Footer optionnel */}
         <Footer />
       </div>
     </div>
