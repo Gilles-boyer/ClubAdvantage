32d2a825d58f840f96f253b7af7087f735e0eabf
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-04-29 13:21:30 +0400

    create: custom updated_at in UserFactory to match last day of creation year

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,6 +24,8 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $createdAt = fake()->dateTimeBetween('-2 years', 'now');
+
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
@@ -33,8 +36,12 @@ class UserFactory extends Factory
             'terms_accepted_at' => now(),
             'status' => fake()->randomElement(['active','inactive','expired']),
             'role_id' => 1,       // à adapter selon les seeder
-            'committee_id' => null
+            'committee_id' => null,
 
+            // On va emetre un created_at aleatoire et l'utiliser pour garder la valeur Y mais changer la valeur M et J.
+            'created_at' => $createdAt,
+            'updated_at' => Carbon::createFromDate($createdAt->format('Y'),12,31),
+            
             // On va recuperer l'id d'un Role au hasard dans la table roles.
             // 'role_id' => Role::pluck('id')->random(),
             // 'committee_id' => null \App\Models\Committee::inRandomOrder()->first()?->id,
