5730b355a2380a688ccc2e4c8efcfebcac24f5a0
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-05-02 10:57:42 +0400

    Create: sanctum ,composer.json and .lock and updated SancFactory and User model

--- database/factories/ScanFactory.php
+++ database/factories/ScanFactory.php
@@ -22,10 +22,23 @@ class ScanFactory extends Factory
             'scanned_at' => fake()->dateTimeBetween('-6 months', 'now'),
             
             // Celui qui scanne : un membre du staff
-            'scanned_by' => User::where('role_id', [1, 2])->inRandomOrder()->first(),
+            'scanned_by' => User::where('role_id', 1)->orwhere('role_id', 2)->inRandomOrder()->first(),
             
             // Celui qui est scanné : un membre CSE
-            'user_id' => User::where('role_id', [3, 4])->inRandomOrder()->first(),
+            'user_id' => User::where('role_id', 3)->orwhere('role_id', 4)->inRandomOrder()->first(),
         ];
     }
 }
+
+/**-----------------Des éventuelle ESSAIS----------------
+ *  
+ * return [
+ *  ...
+ * // // Celui qui scanne : un membre du staff
+ * 'scanned_by' => User::all()->where('role_id', 2)->random()->first(),
+            
+ * // // Celui qui est scanné : un membre CSE
+ * 'user_id' => User::where('role_id', 3)->orWhere('role_id', 4)->first(),
+ *  ...
+ * ];
+*/
