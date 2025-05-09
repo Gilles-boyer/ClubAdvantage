390d3831a57cc4e2520e04dc928e57b4c9f437df
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-05-09 13:43:42 +0400

    Refactored API routes to use apiResource for roles, users, offers, and committees

--- routes/api.php
+++ routes/api.php
@@ -18,29 +18,16 @@ Route::post('/categories', [CategoryController::class, 'store']);
 Route::put('/categories/{category}', [CategoryController::class, 'update']);
 Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
 
-Route::get('/roles', [RoleController::class, 'index']);
-Route::get('/roles/{role}', [RoleController::class, 'show']);
-Route::post('/roles', [RoleController::class, 'store']);
-Route::put('/roles/{role}', [RoleController::class, 'update']);
-Route::delete('/roles/{role}', [RoleController::class, 'destroy']);
+// Cette seule ligne génère automatiquement toutes les routes classiques (index, show, store, update, destroy)
+Route::apiResource('roles', RoleController::class);
+    // Route::get('/roles', [RoleController::class, 'index']);
+    // Route::get('/roles/{role}', [RoleController::class, 'show']);
+    // Route::post('/roles', [RoleController::class, 'store']);
+    // Route::put('/roles/{role}', [RoleController::class, 'update']);
+    // Route::delete('/roles/{role}', [RoleController::class, 'destroy']);
 
-Route::get('/offers', [OfferController::class, 'index']);
-Route::get('/offers/{offer}', [OfferController::class, 'show']);
-Route::post('/offers', [OfferController::class, 'store']);
-Route::put('/offers/{offer}', [OfferController::class, 'update']);
-Route::delete('/offers/{offer}', [OfferController::class, 'destroy']);
+Route::apiResource('offers', OfferController::class);
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
+Route::apiResource('users', UserController::class);
 
-
-// Cette seule ligne génère automatiquement toutes les routes REST classiques (index, show, store, update, destroy)
 Route::apiResource('committees', CommitteeController::class);
-    // Route::get('/committees', [CommitteeController::class, 'index']);
-    // Route::get('/committees/{committee}', [CommitteeController::class, 'show']);
-    // Route::post('/committees', [CommitteeController::class, 'store']);
-    // Route::put('/committees/{committee}', [CommitteeController::class, 'update']);
-    // Route::delete('/committees/{committee}', [CommitteeController::class, 'destroy']);
