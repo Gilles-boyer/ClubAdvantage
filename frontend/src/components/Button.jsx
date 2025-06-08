526700c49f55e42f3cac9e39d2995102f5b87dac
NathanVienne <nathan.vienne@outlook.fr>
2025-06-08 12:18:10 +0400

    test for refactoring buttons

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,16 +1,28 @@
 import clsx from "clsx";
 import Icon from "@mdi/react";
-import { mdilPencil } from '@mdi/light-js';
+import { mdilPencil, mdilDelete } from '@mdi/light-js';
 
-export default function Button({ className = "", type, action, onAction }) {
-  const Icn = (type) => {
-    if(type === )
+
+export default function Button({ type, action, onAction }) {
+  const ICN = (type) => {
+    if(type === 'update'){
+      return (
+            <Icon path={mdilPencil} 
+            size={1.2}/>
+      )
+    }
+    if(type === 'delete'){
+      return (
+            <Icon path={mdilDelete} 
+            size={1.2}/>
+      )
+    }
   }
   return (
     <button
-      onClick={() => onAction}
-      className={clsx(['btn'], className)}
-    >{action}
+      onClick={onAction}
+      className={clsx(['btn',type==='update' && 'btn-success p-1', type==='delete' && 'btn-error p-1'])}
+    >{ICN(type)}{action}
     </button>
   );
 }
--- frontend/src/views/Role.jsx
+++ frontend/src/views/Role.jsx
@@ -1,8 +1,11 @@
+import Button from "../components/Button"
 import Roles from "../components/Role/Roles"
 
 export default function Role() {
     return <>
         <h2 className="text-center font-medium bg-accent py-4 w-full">Roles</h2>
         <Roles />
+        <Button type={'update'} action={'UPDT'} onAction={() => ('')}/>
+        <Button type={'delete'} action={'DLT'} onAction={() => ('')}/>
     </>
 }
