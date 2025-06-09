684d81b8b6afc46344343582191f7d6159257740
NathanVienne <nathan.vienne@outlook.fr>
2025-06-09 16:55:45 +0400

    Login page modified

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import LoginForm from "../components/Login/loginForm";
-import logo from "/logo.png";
+import logo from "/logo_test.png";
 
 export default function Login() {
+    const [hidden, setHidden] = useState(false)
     return (
         <>
-            <div className="flex justify-center items-center min-h-[calc(100vh-64px)]">
+            <div className="flex justify-center items-center bg-neutral min-h-[calc(100vh-0px)]">
                 <div className="flex gap-8 items-center">
-                    <img src={logo} alt="Logo ClubAdvantage" className="h-auto" />
-                    <LoginForm />
+                    <img src={logo} alt="Logo ClubAdvantage" className="h-150" onClick={() => setHidden(!hidden)}/>
+                    {hidden && (<LoginForm setHidden={hidden}/>)}
                 </div>
             </div>
         </>
