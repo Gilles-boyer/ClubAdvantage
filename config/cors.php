40d77a2def404528f8667c0b518ef2054b35110d
NathanVienne <nathan.vienne@outlook.fr>
2025-06-09 16:17:47 +0400

    Cors file modified

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://192.168.1.38:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'http://192.168.1.38:5173', 'http://192.168.1.52:5173'],
 
     'allowed_origins_patterns' => [],
 
