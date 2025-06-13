cafa48b2feb1dee818a23baefe36037daa28a928
NathanVienne <nathan.vienne@outlook.fr>
2025-06-13 15:46:10 +0400

    button added in login view

--- frontend/src/components/ErrorBoundary.jsx
+++ frontend/src/components/ErrorBoundary.jsx
@@ -1,8 +1,13 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
 export function ErrorBoundary() {
     const error = useRouteError();
+    const navigate = useNavigate()
+    const handleHome = () => {
+        navigate('/')
+    }
 
     if (isRouteErrorResponse(error)) {
         return (
@@ -12,7 +17,7 @@ export function ErrorBoundary() {
                         {error.status} {error.statusText}
                     </h1>
                     <p>{error.data}</p>
-                    <Button path={"/"} className="bg-secondary text-white mt-5 py-2 px-3 rounded" action="Retour à l'accueil"/>
+                    <Button path={"/"} className="bg-secondary text-white mt-5 py-2 px-3 rounded" label="Retour à l'accueil" onAction={handleHome} />
                 </div>
             </>
         );
--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -1,7 +1,13 @@
 import LoginForm from "../components/Login/LoginForm";
 import logo from "/Logo_doré.png";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+    const navigate = useNavigate()
+    const handleHome = () => {
+        navigate('/')
+    }
     return (
         <>
             <section className="min-h-screen flex flex-col items-center justify-center bg-neutral py-5">
@@ -10,6 +16,9 @@ export default function Login() {
                     <div className="flex flex-col md:flex-row md:gap-8 items-center pb-5">
                         <img src={logo} alt="Logo ClubAdvantage" className="w-35 md:w-130 object-contain" />
                         < LoginForm />
+                    </div>
+                    <div className="fixed left-200">
+                        <Button path={"/"} className="bg-secondary text-white mt-5 py-2 px-3 rounded" label="Retour à l'accueil" onAction={handleHome} />
                     </div>
                 </div>
             </section>
