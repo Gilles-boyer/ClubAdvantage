d115c56b79d5b7259992cbb01f980e04184c3104
DimMage-974 <dimitri.gravina@hotmail.fr>
2025-07-06 22:44:35 +0400

    RAS corse

--- config/cors.php
+++ config/cors.php
@@ -4,17 +4,15 @@ return [
 
     // Cross-Origin Resource Sharing (CORS) Configuration
 
-    'paths'              => ['api/*','api/stats','login', 'logout', 'sanctum/csrf-cookie', '/auth/validate'],
+    'paths'              => ['api/*','api/stats','login', 'logout', 'sanctum/csrf-cookie', 'auth/validate'],
 
     'allowed_methods'    => ['*'],
 
     'allowed_origins'    => [
         'http://localhost:5173',
-        'http://192.168.1.17:5173',
-        'http://192.168.1.27:5173',
-        'http://192.168.1.28:5173',
-        'http://192.168.1.220:5173',
-        'http://localhost:5173',
+        'http://192.168.1.19:5173',
+        'http://127.0.0.1:5173',
+        'http://192.0.0.1:5173',
         'https://club-avantage.clicksolutions.re'
         // ajoutez ici toutes les URLs front
     ],
